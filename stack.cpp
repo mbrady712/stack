9b04e57faba1ca1331902e7e2952c05f4ef44596
Michael Brady <michaelbrady712@gmail.com>
2023-02-24 21:46:04 -0600

    Created constructor for Stack class.

--- stack.cpp
+++ stack.cpp
@@ -12,6 +12,20 @@ Purpose: This program creates a stack ADT that holds a collection of pointers to
 // each cpp includes ONLY it's header directly
 #include "stack.h"
 
-/*
- * write all your stack methods here
- */
+//accepts an int for the stack size
+Stack::Stack(int arg){
+    top = BOTTOM;
+
+    if(arg >= 2){
+        size = arg;
+    }else{
+        size = 10;
+    }
+
+    std::cout << size;
+
+    stack = new Data*[size];
+}
+
+
+
