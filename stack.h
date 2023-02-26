d5743918f2cc00fe6eb6e2d8f05d46d4b1c2f3ad
Michael Brady <michaelbrady712@gmail.com>
2023-02-25 19:05:37 -0600

    Removed unneccessary comments

--- stack.h
+++ stack.h
@@ -24,18 +24,9 @@ public:
     int getSize();
 
 private:
-
-    /* 
-     * write all your private method prototypes here
-     */
-
-    // these are the only attributes you need.
-    // do not modify them or add any other attributes
     int top;
     int size;
     Data **stack;
-    // notice stack is a pointer to a pointer. That means if you do
-    // a single array allocation, you will get an array of pointers
 };
 
 #endif //STACK_H
